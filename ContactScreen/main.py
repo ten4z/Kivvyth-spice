af420186cea1340da99f292d01269fa6b72ce89f
Josiel Soares <115186280+ten4z@users.noreply.github.com>
2023-06-05 17:00:03 -0300

    Add files via upload

--- ContactScreen/main.py
+++ ContactScreen/main.py
@@ -1,43 +1,16 @@
 import kivy
-import sqlite3
 import webbrowser
-from plyer import tts
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from datetime import datetime
-from kivymd.toast import toast
-from kivy.uix.popup import Popup
-from kivy.core.window import Window
-from kivymd.uix.tab import MDTabsBase
-from kivymd.theming import ThemeManager
-from kivy.uix.behaviors import FocusBehavior
-from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.list import OneLineIconListItem
-from kivy.uix.recyclegridlayout import RecycleGridLayout
-from kivy.properties import ListProperty, NumericProperty, ObjectProperty, BooleanProperty
-from kivmob import KivMob,TestIds,RewardedListenerInterface
-from kivy.uix.recycleview.views import RecycleDataViewBehavior
-from kivymd.uix.behaviors.toggle_behavior import MDToggleButton
-from kivy.uix.recycleview.layout import LayoutSelectionBehavior
-from kivymd.uix.button import MDRoundFlatButton, MDFillRoundFlatButton, MDRectangleFlatButton
-
-from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.image import Image
-
-
+from kivymd.uix.screen import MDScreen
+from kivy.properties import ObjectProperty
+from kivymd.uix.scrollview import MDScrollView
+from kivy.uix.behaviors import ButtonBehavior
+from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.behaviors import CircularRippleBehavior
 
-
-from kivymd.uix.screenmanager import MDScreenManager
-from kivymd.uix.scrollview import MDScrollView
-from kivymd.uix.screen import MDScreen
-import webbrowser
-import sqlite3
-
 Builder.load_file("gui.kv")
-
-
 
 class CircularRippleButton(CircularRippleBehavior, ButtonBehavior, Image):
     def __init__(self, **kwargs):
@@ -47,20 +20,14 @@ class CircularRippleButton(CircularRippleBehavior, ButtonBehavior, Image):
 class Sc_Manager(MDScreenManager):
 	pass
 
-class Sc_Menu(MDScreen):	
+class Sc_Contact(MDScreen):	
 	def __init__(self, **kwargs):
-		super(Sc_Menu, self).__init__(**kwargs)
+		super(Sc_Contact, self).__init__(**kwargs)
 		self.current_app = MDApp.get_running_app()
 		
-class ContentNavigationDrawer(MDScrollView):
-	
+class ContentNavigationDrawer(MDScrollView):	
 	nav_drawer = ObjectProperty()
            
-class Sc_About(MDScreen):
-	pass  
-
-class Sc_Info(MDScreen):
-	pass  
 
 class ContentManager(MDScreen):
 	pass
